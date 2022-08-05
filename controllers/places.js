9fd6c4a911cb415056b8bfcd1efe8501d93b73f8
Drake Owen <drakeowen@drakes-mbp.lan>
2022-08-05 19:37:43 -0400

    Adding bootstrap to index page

--- controllers/places.js
+++ controllers/places.js
@@ -4,18 +4,19 @@ const router = require('express').Router()
 
 router.get('/', (req, res) => {
     let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: 'http://placekitten.com/250/250'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: 'http://placekitten.com/250/250'
-      }]
+  name: 'H-Thai-ML',
+  city: 'Seattle',
+  state: 'WA',
+  cuisines: 'Thai, Pan-Asian',
+  pic: '/images/h-thai-ml-tables.jpeg'
+}, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: '/images/coffee-cat.jpeg'
+}]
+
       
     res.render('places/index', {places})
 })
--- views/error404.jsx
+++ views/error404.jsx
@@ -7,6 +7,9 @@ function error404 () {
           <main>
               <h1>404: PAGE NOT FOUND</h1>
               <p>Oops, sorry, we can't find this page!</p>
+              <p>Enjoy this kitten photo instead of what you were looking for!</p>
+              <img src="https://placekitten.com/500/500" alt="Picture of a kitten"/>
+              <p>Image from placekitten.com</p>
           </main>
       </Def>
     )
