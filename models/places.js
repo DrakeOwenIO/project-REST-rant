9715c32df497b1d15ec1644c5b0eaed6b0093e88
Drake Owen <drakeowen@drakes-mbp.lan>
2022-08-05 23:37:22 -0400

    Can add new places

---
+++ models/places.js
@@ -0,0 +1,13 @@
+module.exports = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: 'public/images/h-thai-ml-tables.jpeg'
+}, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: 'public/images/coffee-cat.jpeg'
+}]
